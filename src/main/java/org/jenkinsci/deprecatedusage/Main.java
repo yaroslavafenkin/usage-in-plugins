7033b497ba5d71daa960cc252b03d91d1ddf17c6
evernat <evernat@free.fr>
2015-11-18 23:06:28 +0100

    avoid IllegalThreadStateException when running in maven

--- src/main/java/org/jenkinsci/deprecatedusage/Main.java
+++ src/main/java/org/jenkinsci/deprecatedusage/Main.java
@@ -11,6 +11,7 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
     public static void main(String[] args) throws Exception {
@@ -66,6 +67,7 @@ public class Main {
                 Log.print(".");
             }
         }
+        executorService.awaitTermination(1, TimeUnit.SECONDS);
         executorService.shutdown();
         log("");
         log("");
